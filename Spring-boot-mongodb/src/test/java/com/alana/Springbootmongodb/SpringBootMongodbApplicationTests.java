ee3ee1b0ee0256df4d87ddfcce9d2c62d76d6c55
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-07 22:12:25 +0100

    first commit

---
+++ Spring-boot-mongodb/src/test/java/com/alana/Springbootmongodb/SpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alana.Springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
