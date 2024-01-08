e8048fc3d5cf453512a697cb906af8746346042a
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-08 06:04:07 +0100

    Expense controller

---
+++ Spring-boot-mongodb/src/main/java/com/alana/Springbootmongodb/controller/ExpenseController.java
@@ -0,0 +1,16 @@
+package com.alana.Springbootmongodb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/expense")
+public class ExpenseController {
+    public void addExpense(){}
+    public void updateExpense(){}
+    public void getAllExpenses(){}
+
+    public void getExpenseByName(){}
+    public void deleteExpense(){}
+
+}
