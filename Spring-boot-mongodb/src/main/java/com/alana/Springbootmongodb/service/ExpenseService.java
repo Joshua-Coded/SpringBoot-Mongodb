2f755f464ac2273a112720593c4c58cd2b4b5124
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-08 06:22:18 +0100

    Expense update and getAllExpenses

--- Spring-boot-mongodb/src/main/java/com/alana/Springbootmongodb/service/ExpenseService.java
+++ Spring-boot-mongodb/src/main/java/com/alana/Springbootmongodb/service/ExpenseService.java
@@ -18,7 +18,17 @@ public class ExpenseService {
     public void addExpense(Expense expense){
         expenseRepository.insert(expense);
     }
-    public void updateExpense(){}
+    public void updateExpense(Expense expense){
+        Expense savedExpense = expenseRepository.findById(expense.getId())
+                .orElseThrow(() -> new RuntimeException(
+                        String.format("Cannot find expense by ID's %s", expense.getId())));
+
+        savedExpense.setExpenseName(expense.getExpenseName());
+        savedExpense.setExpenseCategory(expense.getExpenseCategory());
+        savedExpense.setExpenseAmount(expense.getExpenseAmount());
+
+        expenseRepository.save(expense);
+    }
     public List<Expense> getAllExpenses(){
         return expenseRepository.findAll();
     }
