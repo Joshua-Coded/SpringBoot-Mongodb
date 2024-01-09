8413b0a57e78107e728a36754cb029e25f8d44cb
joshbam-joshwilly <j.alana@alustudent.com>
2024-01-09 04:51:23 +0100

    movie api developed with spring boot

---
+++ moviesApi/src/main/java/com/alana/moviesApi/Movie.java
@@ -0,0 +1,29 @@
+package com.alana.moviesApi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+}
---
+++ moviesApi/src/test/java/com/alana/moviesApi/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alana.moviesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
